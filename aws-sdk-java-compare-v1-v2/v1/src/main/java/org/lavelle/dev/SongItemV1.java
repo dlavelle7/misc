d18cba8625b0f98845273e503cd58df9e5723000
David Lavelle <david.lavelle@genesys.com>
2020-10-15 17:00:14 +0100

    v1 working also

--- aws-sdk-java-compare-v1-v2/v1/src/main/java/org/lavelle/dev/SongItemV1.java
+++ aws-sdk-java-compare-v1-v2/v1/src/main/java/org/lavelle/dev/SongItemV1.java
@@ -1,8 +1,8 @@
 package org.lavelle.dev;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
 
 @DynamoDBTable(tableName="Songs")
 public class SongItemV1 {
@@ -30,5 +30,12 @@ public class SongItemV1 {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @DynamoDBIgnore
+    public static DynamoDBMapper dynamoDBMapper() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDBMapper dbMapper = new DynamoDBMapper(client);
+        return dbMapper;
     }
 }
--- aws-sdk-java-compare-v1-v2/v1/src/main/java/org/lavelle/dev/V1Handler.java
+++ aws-sdk-java-compare-v1-v2/v1/src/main/java/org/lavelle/dev/V1Handler.java
@@ -1,23 +1,50 @@
 package org.lavelle.dev;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class V1Handler implements RequestHandler<Object, Object> {
 
     private static final Logger LOGGER = Logger.getLogger("handler");
+    private DynamoDBMapper dbMapper;
 
     public V1Handler() {
         LOGGER.info("V1 Lambda handler initializing");
+        dbMapper = SongItemV1.dynamoDBMapper();
     }
 
     @Override
     public Object handleRequest(Object o, Context context) {
         // TODO: Create new lambda for V1 comparison, assign IAM role to access dynamodb songs table
-        // TODO: Implement V1 DynamoDBMapper for Songs dynamoDB table
-        LOGGER.info("V1 Lambda handler invoked");
+        try {
+            LOGGER.info("V1 Lambda handler invoked");
+
+            // PUT item
+            SongItemV1 songToPut = new SongItemV1();
+            songToPut.setArtist("Lavelle");
+            songToPut.setTitle("Howling Wind");
+
+            LOGGER.info(String.format("Putting Song Item: %s, %s", songToPut.getArtist(), songToPut.getTitle()));
+            dbMapper.save(songToPut);
+
+            // GET item
+            SongItemV1 songToGet = new SongItemV1();
+            songToGet.setArtist("Black Sabbath");
+            songToGet.setTitle("Paranoid");
+
+            LOGGER.info("Get Song Item");
+            SongItemV1 songRetrieved = dbMapper.load(songToGet);
+
+            LOGGER.log(Level.INFO, String.format("Retrieved item from DynamoDb: Artist = %s, Song = %s",
+                    songRetrieved.getArtist(), songRetrieved.getTitle()));
+        }
+        catch (Exception e) {
+            LOGGER.log(Level.WARNING , "Lambda execution error", e);
+        }
         return "OK";
     }
 }
